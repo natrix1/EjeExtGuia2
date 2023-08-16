272bab670c67bce0603c97cecf928304d05ddb43
natrix1 <141676108+natrix1@users.noreply.github.com>
2023-08-16 16:01:04 -0300

    Initial commit

---
+++ src/ejeextguia2/EjeExtGuia2.java
@@ -0,0 +1,27 @@
+/*
+Desarrolla una aplicación de lista de tareas pendientes utilizando interfaces gráficas y colecciones
+en Java. La interfaz gráfica debe permitir al usuario agregar nuevas tareas, marcar tareas como
+completadas y eliminar tareas de la lista. Además, la aplicación debe mostrar la lista de tareas
+pendientes y la lista de tareas completadas. Utiliza una colección adecuada para almacenar las
+tareas y asegúrate de que la interfaz gráfica se actualice correctamente cuando se realicen cambios
+en la lista.
+Es importante que repartan las tareas entre los miembros del equipo, de tal forma que: uno crea
+repositorio en GitHub; los demás clonan, luego cada uno hace una de las clases que forman parte
+del proyecto y suben al repositorio los cambios.
+ */
+package ejeextguia2;
+
+/**
+ *
+ * @author Altamirano
+ */
+public class EjeExtGuia2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
